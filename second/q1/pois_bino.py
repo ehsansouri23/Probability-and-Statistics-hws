458007e72a37f8211025832225e8d3f905567feb
Ehsan <ehsansouri23@gmail.com>
2019-01-18 19:19:03 +0330

    add x1+x2 and x2+x3 charts

--- second/q1/pois_bino.py
+++ second/q1/pois_bino.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import math as mp
 
 
@@ -12,3 +14,27 @@ def binomial(x, n, p):
     else:
         a = 0
     return a
+
+
+x1 = 0
+x2 = 0
+x3 = 0
+
+plt.figure(1)
+plt.subplot(211)
+plt.title("x1 + x2")
+for i in range(0, 150):
+    n1 = binomial(i, 100, 0.3)
+    n2 = binomial(i, 100, 0.5)
+    m = n1 + n2
+    plt.scatter(i, m)
+
+plt.subplot(212)
+plt.title("x2 + x3")
+for i in range(0, 150):
+    n1 = binomial(i, 100, 0.5)
+    n2 = binomial(i, 200, 0.5)
+    m = n1 + n2
+    plt.scatter(i, m)
+
+plt.show()
